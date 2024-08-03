68b5d8be1f60084c193cdf4152ff99187963fc01
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-08-03 18:17:32 +0500

    script added to remove the loader after 2 seconds

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var micO = false
 var headO = false
 
+window.onload = function() {
+    setTimeout(function() {
+      var loader = document.getElementById('loader');
+      if (loader) {
+        loader.remove();
+      }
+    }, 2000);
+  };
+
 function mic(){
     if(!micO){
         document.getElementById("mic").className = "fa-solid fa-microphone-slash mute"
