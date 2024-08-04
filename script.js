472137d02c80c6432df5ba15aa23ca3909d198c8
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-08-04 19:52:10 +0500

    voice added on mute and unmute

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 var micO = false
 var headO = false
 
-window.onload = function() {
-    setTimeout(function() {
-      var loader = document.getElementById('loader');
-      if (loader) {
-        loader.remove();
-      }
+window.onload = function () {
+    setTimeout(function () {
+        var loader = document.getElementById('loader');
+        if (loader) {
+            loader.remove();
+        }
     }, 2000);
-  };
+};
 
-function mic(){
-    if(!micO){
-        document.getElementById("mic").className = "fa-solid fa-microphone-slash mute"
-        micO = true
-    }else{
-        document.getElementById("mic").className = "fa-solid fa-microphone"
-        micO = false
+var x = new Audio("./Assets/discordmute_IZNcLx2.mp3"); // Make sure this URL points directly to an audio file
+var y = new Audio("./Assets/discord-unmute-sound.mp3"); // Make sure this URL points directly to an audio file
+
+function mic() {
+    if (!micO) {
+        x.play();
+        document.getElementById("mic").className = "fa-solid fa-microphone-slash mute";
+        micO = true;
+    } else {
+        y.play();
+        document.getElementById("mic").className = "fa-solid fa-microphone";
+        micO = false;
     }
 }
-function headp(){
-    if(!headO){
+
+function headp() {
+    if (!headO) {
+        x.play();
         document.getElementById("head").className = "fa-solid fa-headphones mute"
         headO = true
-    }else{
+    } else {
         document.getElementById("head").className = "fa-solid fa-headphones"
+        y.play();
         headO = false
     }
 }
